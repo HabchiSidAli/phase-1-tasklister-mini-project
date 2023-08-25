723a7bc52988947884b63aacbf68ff5b8234e9da
HabchiSidAli <sidalihabchi96@gmail.com>
2023-08-25 01:38:34 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleToDo(document.getElementById("new-task-description").value)
+  })
+
+  function handleToDo (toDo) {
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerHTML = "x";
+    button.addEventListener("click", (e) => {
+      //deleting on button press
+      e.target.parentNode.remove();
+    });
+    li.innerHTML = toDo + " ";
+    li.appendChild(button);
+    document.getElementById("tasks").appendChild(li);   
+  }
 });
